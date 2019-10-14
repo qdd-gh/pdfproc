adda98bea740a9df5cea0b1ad5980cb2eba05a32
qdd-gh <55011673+qdd-gh@users.noreply.github.com>
2019-10-14 10:17:54 -0400

    Allow version number override

--- pdfslice/__init__.py
+++ pdfslice/__init__.py
@@ -1,5 +1,6 @@
 """PDF splitting for Python"""
 
 from datetime import date
+import os
 
-__version__ = date.today().strftime("%Y.%m.%d")
+__version__ = os.environ.get("PDFSLICE_VERSION", date.today().strftime("%Y.%m.%d"))
