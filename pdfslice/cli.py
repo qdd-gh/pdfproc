3f2cc92eb293adb1ec5d1cf8857fdcf234606f21
qdd-gh <55011673+qdd-gh@users.noreply.github.com>
2019-10-14 13:48:00 -0400

    Add documentation for cli.py

--- pdfslice/cli.py
+++ pdfslice/cli.py
@@ -8,7 +8,14 @@ from pdfslice import slice
 @click.command()
 @click.argument("infile", type=click.File("rb"))
 @click.argument("prefix", type=click.STRING)
-def pdfslice(infile: typing.BinaryIO, prefix: str):
+def pdfslice(infile: typing.BinaryIO, prefix: str) -> None:
+    """Split an N-page PDF into N individual PDFs.
 
+    e.g., INFILE.pdf -> [PREFIX-1.pdf, PREFIX-2.pdf, ..., PREFIX-N.pdf]
+    \f
+
+    :param infile: An open file-like object containing the original PDF
+    :param prefix: The base name used for each output file. Does not include the file extension
+    """
     for split in slice.filesplit(infile, prefix):
         split.filename.write_bytes(split.page.read())
